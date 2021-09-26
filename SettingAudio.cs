f68944c9efe91b5cb1f92a220a7b73b9aa67a997
Wwchoc <2855236293@qq.com>
2021-09-26 16:19:50 +0800

    my test

---
+++ SettingAudio.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingAudio : MonoBehaviour
+{
+    public CanvasGroup go;
+
+    public Slider bgmslider;
+    public Toggle bgmtoggle;
+
+    public Slider soundlider;
+    public Toggle soundtoggle;
+
+    private void Update()
+    {
+        AudioManager.Instance.SetBGM(bgmslider.value);
+        AudioManager.Instance.SetSound(soundlider.value);
+
+        if (bgmtoggle.isOn == false)
+        {
+            AudioManager.Instance.SetBGM(-80f);
+            bgmslider.interactable = false;
+        }
+        else { bgmslider.interactable = true; }
+
+        if (soundtoggle.isOn == false)
+        {
+            AudioManager.Instance.SetSound(-80f);
+            soundlider.interactable = false;
+
+        }
+        else { soundlider.interactable = true; }
+
+    }
+    public void open()
+    {
+        go.blocksRaycasts = true;
+        go.alpha = 1;
+    }
+    public void close()
+    {
+        go.blocksRaycasts = false;
+        go.alpha = 0;
+    }
+
+
+
+}
